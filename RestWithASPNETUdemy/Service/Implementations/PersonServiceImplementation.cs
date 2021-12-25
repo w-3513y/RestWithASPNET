2b70091f14912ee3e7f77b731ee2cfcf649db537
Wesley <wesleyferreirasb@gmail.com>
2021-12-25 18:30:48 -0300

    S7A4 - Implementando a interface PersonService

---
+++ RestWithASPNETUdemy/Service/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,36 @@
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations;
+
+public class PersonServiceImplementation : IPersonService
+{    
+
+    public Person Create(Person person) => person;
+
+    public void Delete(long id)
+    {
+    }
+
+    public List<Person> FindAll()
+    {
+        var people = new List<Person>();        
+        for (int i = 0; i < 8; i++){
+            people.Add(FindByID(i));
+        }
+        return people;
+    }
+
+    public Person FindByID(long id)
+    {
+        return new Person
+        {
+            Id = id,
+            FirstName = $"Name{id}",
+            LastName = $"LastName{id}",
+            Adress = $"Adress{id} - UF",
+            Gender = "Gender"
+        };
+    }
+
+    public Person Update(Person person) => person;
+}
