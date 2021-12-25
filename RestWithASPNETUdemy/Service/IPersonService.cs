63c7f91dfc96d10cc5fc015667ecbbf7aa6b3bd5
Wesley <wesleyferreirasb@gmail.com>
2021-12-25 18:20:18 -0300

    S7A3 - Inclusão da pasta interfaces

---
+++ RestWithASPNETUdemy/Service/IPersonService.cs
@@ -0,0 +1,11 @@
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services;
+
+public interface IPersonService{
+    Person Create(Person person);
+    Person FindByID(long id);
+    List<Person> FindAll();
+    Person Update(Person person);
+    void Delete(long id);
+}
