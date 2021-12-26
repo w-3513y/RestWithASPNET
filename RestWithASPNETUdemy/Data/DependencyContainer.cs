e442c7d7471e9b381f00d5e9ff550eb5b834236c
Wesley <wesleyferreirasb@gmail.com>
2021-12-26 10:37:42 -0300

    injeção de dependencia do context no dependency container

--- RestWithASPNETUdemy/Data/DependencyContainer.cs
+++ RestWithASPNETUdemy/Data/DependencyContainer.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
 
 namespace RestWithASPNETUdemy.Data;
 
 public class DependencyContainer
-{
-    public static void RegisterServices(IServiceCollection services)
+{    
+   
+    public static void RegisterServices(WebApplicationBuilder builder)
     {
-        services.AddScoped<IPersonService, PersonServiceImplementation>();
+        var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+        builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 27))));
+        builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
     }
 }
--- RestWithASPNETUdemy/Program.cs
+++ RestWithASPNETUdemy/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-DependencyContainer.RegisterServices(builder.Services);
+DependencyContainer.RegisterServices(builder);
 
 var app = builder.Build();
 
